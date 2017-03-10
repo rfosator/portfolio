026daf7cc134be1d9790abbb1d8ddbeb21824aa6
rfosator <ramonft@gmail.com>
2017-03-10 15:25:49 -0600

    Agregar archivos de proyecto.

---
+++ ProfilePortfolio/wwwroot/js/run.js
@@ -0,0 +1,10 @@
+﻿angular.module('profiler', ['ngRoute'])
+    .config(['$routeProvider', '$locationProvider',
+        function ($routeProvider, $locationProvider) {
+            $locationProvider.hashPrefix('');
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'js/views/inicio.html'
+                });
+        }
+    ]);  
