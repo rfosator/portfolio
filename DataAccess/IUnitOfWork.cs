1a4e1ffeddcb7141873b1f70d411b7cf3cdea682
rfosator <ramonft@gmail.com>
2017-03-17 15:13:15 -0600

    Se agrega IoC container, UnitOfWork y modificaciones API

---
+++ DataAccess/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace Profile.Data
+{
+    public interface IUnitOfWork
+    {
+        IRepository<TEntity> Get<TEntity>()
+            where TEntity : class;
+    }
+}
--- ProfilePortfolio/wwwroot/js/components/categories.js
+++ ProfilePortfolio/wwwroot/js/components/categories.js
@@ -4,6 +4,7 @@
         var ctrl = this;
 
         ctrl.items = [];
+        ctrl.loading = false;
 
         ctrl.attach = function () {
             var content = {
@@ -15,9 +16,10 @@
             });
         };
 
-        ctrl.update = function (c) {
-            $http.put("api/Categories/" + c.id, c).then(function () {
-                console.log('updated');
+        ctrl.update = function (selected) {
+            ctrl.loading = true;
+            $http.put("api/Categories/" + selected.id, selected).then(function () {
+                ctrl.loading = false;
             });
         };
 
